7d3fc5537b6b75aa455929c0785e6ef9fc734a39
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2025-11-09 22:33:14 +0700

    Simplify ProductSeeder

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -2,10 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB; 
-use App\Models\Product; 
+use App\Models\Product;
 
 class ProductSeeder extends Seeder
 {
@@ -13,32 +11,37 @@ class ProductSeeder extends Seeder
     {
         // Product::query()->delete();
 
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        Product::truncate();
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
-
-        Product::create([
+        $products =[
+            [
             'name'        => 'Keyboard Mechanical 60%',
             'description' => 'Keyboard mechanical compact untuk gaming dan kerja.',
             'price'       => 550000,
             'stock'       => 20,
             'sku'         => 'KB-60-MECH',
-        ]);
+            ],
 
-        Product::create([
+            [        
             'name'        => 'Mouse Wireless Silent',
             'description' => 'Mouse wireless dengan klik senyap dan baterai awet.',
             'price'       => 175000,
             'stock'       => 50,
             'sku'         => 'MS-WL-SILENT',
-        ]);
+            ],
 
-        Product::create([
+            [
             'name'        => 'Monitor 24 inch IPS',
             'description' => 'Monitor 24 inch Full HD panel IPS.',
             'price'       => 1500000,
             'stock'       => 10,
             'sku'         => 'MN-24-IPS',
-        ]);
+        ],
+    ];
+
+    foreach ($products as $data) {
+        Product::updateOrCreate(
+            ['sku' => $data['sku']],  // key unik
+            $data
+        );
     }
+}
 }
